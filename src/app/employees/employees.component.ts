2c240771b0db4633e8dc5a97af7a475fb230e30a
Edward Tanguay (windows) <edward@tanguay.info>
2023-02-24 16:14:30 +0100

    api employees in

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { IEmployee } from 'src/shared/interfaces';
+import axios from 'axios';
 
 @Component({
-  selector: 'app-employees',
-  templateUrl: './employees.component.html',
-  styleUrls: ['./employees.component.scss']
+	selector: 'app-employees',
+	templateUrl: './employees.component.html',
+	styleUrls: ['./employees.component.scss']
 })
 export class EmployeesComponent {
+	employees: IEmployee[] = [];
+	constructor() {
+		(async () => {
+			const rawEmployees = (await axios.get('https://edwardtanguay.vercel.app/share/employees.json')).data;
 
+			rawEmployees.forEach((rawEmployee: any) => {
+				this.employees.push({
+					firstName: rawEmployee.firstName,
+					lastName: rawEmployee.lastName,
+					notes: rawEmployee.notes
+				})
+			})
+
+		})();
+	}
 }
